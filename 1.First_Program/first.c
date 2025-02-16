9dcbcad60549e363ec2835877a0b14d114227311
shubham <shubhamshetkar46@gmail.com>
2025-02-17 01:00:10 +0530

    update

---
+++ 1.First_Program/first.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    
+    printf("Hello, World!\n");
+    printf("just a sample project");
+    
+    return 0; 
+}
+
+/*
+#include
+It is the preprocessor and the first word read by the compiler
+
+stdio.h
+It is the header file.
+
+int main()
+main() function with return type int.
+
+printf
+Used to print the formatted string to stdout. The standard output is stdout.
+
+return
+Terminates the function execution and the return statement returns the control to the calling function.
+*/
+
+/*
+TO RUN THIS ON TERMINAL FIRST COMPILE IT AND THEN RUN
+gcc file.c -o name
+name.exe
+*/
