63d793627d2af38a40e9b98b8747ea4eb0708162
shubham <shubhamshetkar46@gmail.com>
2025-02-21 11:45:39 +0530

    update

---
+++ 9.Decision_Making/6.Ternary.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){ 
+    int a = 345;
+    int b= 345452;
+ // condition ? expression-if-true : expression-if-false
+    a>b?printf("a is greater"):printf("b is greater");
+}
