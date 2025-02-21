55f02c5632236cb5f32a4c33afd8a16d2776c18d
shubham <shubhamshetkar46@gmail.com>
2025-02-21 11:47:00 +0530

    update

---
+++ 10.Practice_Problem-03/problem1.c
@@ -0,0 +1,20 @@
+/*
+What will be the output of this program 
+int a = 10; 
+if (a = 11) 
+    printf("I am 11"); 
+else 
+    printf("I am not 11");
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    if (a = 11)
+        printf("I am 11"); 
+    else
+        printf("I am not 11"); 
+    return 0;
+}
